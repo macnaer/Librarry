b624e938b245e51fb35dd12ce5c9b3df4dba1388
trofimchuk <trofimchuk@itstep.academy>
2021-08-18 09:56:31 +0300

    Add project files.

---
+++ Librarry/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Librarry.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Librarry.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        DbSet<Book> Books { get; set; }
+    }
+}
